28f3b2aa18adec8a4268a3872984e2128c41d8c8
Gerkiz <fs@nvfs.se>
2022-06-08 23:21:48 +0200

    minor fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ controlFile="control.lua"
 originalSum=$(sha256sum "$controlFile" | awk '{print $1}')
 grep -rl "ENABLE MAPS HERE" -A 500 "$controlFile" | grep -v "ENABLE MAPS HERE" | xargs sed -i "s/^require 'maps/--require 'maps/g"
 changedSum=$(sha256sum "$controlFile" | awk '{print $1}')
-changedDiffers=$(echo "$originalSum" "$controlFile" | sha256sum -c > /dev/null 2>&1)
 
 echo "Original file sum:" "$originalSum"
 echo "Changed file sum:" "$changedSum"
 
-if (echo "$changedDiffers" | grep -q "OK" ) ; then
+
+if (echo "$originalSum" "$controlFile" | sha256sum -c | grep -q "OK" > /dev/null 2>&1 ) ; then
   echo "Control does not differ"
   echo "::set-output name=should_run::0"
 else
