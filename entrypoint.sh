88f58f5452939823bdfaac45b923fc68f76da02e
Gerkiz <fs@nvfs.se>
2022-06-08 21:32:18 +0200

    minor fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,15 +3,14 @@
 # Comment all maps that might be required
 set -f
 controlFile="control.lua"
-originalsum=$(sha256sum $controlFile | awk '{print $1}')
-grep -rl "ENABLE MAPS HERE" -A 500 $controlFile | grep -v "ENABLE MAPS HERE" | xargs sed -i 's/^require \x27maps/--require \x27maps/g'
-changedsum=$(echo $originalsum $controlFile | sha256sum -c)
+originalSum=$(sha256sum $controlFile | awk '{print $1}')
+grep -rl "ENABLE MAPS HERE" -A 500 $controlFile | grep -v "ENABLE MAPS HERE" | xargs sed -i "s/^require 'maps/--require 'maps/g"
+changedSum=$(echo "$originalSum" $controlFile | sha256sum -c)
 
-if echo "$changedsum" | grep -q "OK" ; then
-	echo "Control does not differ"
-	echo "::set-output name=should_run::0"
+if (echo "$changedSum" | grep -q "OK") ; then
+  echo "Control does not differ"
+  echo "::set-output name=should_run::0"
 else
-	echo "Control differs - push is needed"
-	echo "::set-output name=should_run::1"
+  echo "Control differs - push is needed"
+  echo "::set-output name=should_run::1"
 fi
-
