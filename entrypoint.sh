b61dc57caf04838f742a4e1b0b82aabad363db96
Gerkiz <fs@nvfs.se>
2022-06-08 22:18:54 +0200

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,15 @@
 
 # Comment all maps that might be required
 controlFile="control.lua"
-originalSum=$(sha256sum $controlFile | awk '{print $1}')
-grep -rl "ENABLE MAPS HERE" -A 500 $controlFile | grep -v "ENABLE MAPS HERE" | xargs sed -i "s/^require 'maps/--require 'maps/g"
-changedSum=$(sha256sum $controlFile | awk '{print $1}')
-changedDiffers=$(echo "$originalSum" $controlFile | sha256sum -c)
+originalSum=$(sha256sum "$controlFile" | awk '{print $1}')
+grep -rl "ENABLE MAPS HERE" -A 500 "$controlFile" | grep -v "ENABLE MAPS HERE" | xargs sed -i "s/^require 'maps/--require 'maps/g"
+changedSum=$(sha256sum "$controlFile" | awk '{print $1}')
+changedDiffers=$(echo "$originalSum" "$controlFile" | sha256sum -c > /dev/null 2>&1)
 
 echo "Original file sum:" "$originalSum"
 echo "Changed file sum:" "$changedSum"
 
-if (echo "$changedDiffers" | grep -q "OK") ; then
+if (echo "$changedDiffers" | grep -q "OK" ) ; then
   echo "Control does not differ"
   echo "::set-output name=should_run::0"
 else
